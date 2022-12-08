d883fd7b91c315e59d74e4d958516b149da680e8
David Wang <114452306+dkiller6893@users.noreply.github.com>
2022-12-07 23:06:04 -0500

    Update and rename app.xhtml to app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, session, request, redirect
+import random
+
+app = Flask(__name__)
+app.secret_key = 'thisIsSecret'
+
+@app.route('/')
+def index():
+	session['number'] = random.randrange(0,100)
+	print(session['number'])
+	return render_template("index.html")
+
+@app.route('/guess', methods=['POST'])
+def result():
+	if request.form['guess'] == session['number']:
+		answer = "Correct"
+		return render_template("index.html", answer=answer)
+	elif request.form['guess'] > session['number']:
+		answer = "Too Low"
+		return render_template("index.html", answer=answer)
+	else:
+		answer = "Too High"
+		return render_template("index.html", answer=answer)
+
+app.run(debug=True)
